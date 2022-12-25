3a2862dfbb34cffed11d9cbcdd92aa474c289ef1
mustafiz-codes <js.mustafiz@gmail.com>
2022-12-25 16:28:45 +0000

    design fix for header saction of home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -17,43 +17,69 @@ const HomeScreen = () => {
     });
   }, []);
 
+  const randomPicture = (type = null) => {
+    let picUrl = "";
+    if (type === "maleThumbnail") {
+      picUrl =
+        "https://randomuser.me/api/portraits/thumb/men/" +
+        Math.floor(Math.random() * 100) +
+        ".jpg";
+    } else if (type === "femaleThumbnail") {
+      picUrl =
+        "https://randomuser.me/api/portraits/thumb/men/" +
+        Math.floor(Math.random() * 100) +
+        ".jpg";
+    } else {
+      picUrl =
+        "https://picsum.photos/200/300?random=" +
+        Math.floor(Math.random() * 100);
+    }
+    console.log("hi " + picUrl);
+    return picUrl;
+  };
+
   return (
-    <SafeAreaView className="bg-black pt-5 px-4">
-      <Text className="mt-8">
+    <SafeAreaView className="bg-zinc-900 px-4 flex-1">
+      <View className="mt-8 w-full">
         {/* header */}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2 w-full">
-          <View className="">
+        <View className="flex pb-3 items-center space-x-2 flex-row">
+          <View className="flex-none">
             <Image
               source={{
-                uri: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngall.com%2Fdelivery-png%2Fdownload%2F101209&psig=AOvVaw3cB7QCkAfqN49XOko2V3nU&ust=1671969725203000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCOjd7d2akvwCFQAAAAAdAAAAABAZ",
+                uri: randomPicture(),
               }}
               className="h-7 w-7 bg-yellow-400 rounded-full"
             />
           </View>
-          <View className="flex-1 w-auto">
-            <Text className="text-gray-200 text-xs">Delivery Now!</Text>
+          <View className="flex-1">
+            <Text className="text-zinc-300 text-xs">Delivery Now!</Text>
             <Text className="font-bold text-white text-xl">
               Current Location
-              <ChevronDownIcon size={20} color="#FDDA0D" />
+              <ChevronDownIcon size={25} color="#FDDA0D" />
             </Text>
           </View>
-          <View className="">
-            <UserIcon size={35} color="#FDDA0D" />
+          <View className="flex-none">
+            <Image
+              source={{
+                uri: randomPicture("maleThumbnail"),
+              }}
+              className="h-7 w-7 bg-yellow-400 rounded-full"
+            />
           </View>
         </View>
 
         {/* search */}
-        <View className="flex-row items-center pb-2 w-full">
-          <View className="flex-row items-center bg-gray-300 rounded-xl px-1">
-            <MagnifyingGlassCircleIcon size={20} color="#FDDA0D" />
+        <View className="flex-row pb-2 items-center space-x-2">
+          <View className="flex-row flex-1 items-center bg-gray-300 rounded-xl px-1">
+            <MagnifyingGlassCircleIcon size={30} color="black" />
             <TextInput
-              placeholder="Restaurant or cuisines"
-              className="flex-1 px-2"
+              placeholder="Restaurants and cuisines"
+              className="flex-1 px-2 py-2"
             />
           </View>
-          <AdjustmentsVerticalIcon color="#FDDA0D" />
+          <AdjustmentsVerticalIcon size={30} color="#FDDA0D" />
         </View>
-      </Text>
+      </View>
     </SafeAreaView>
   );
 };
